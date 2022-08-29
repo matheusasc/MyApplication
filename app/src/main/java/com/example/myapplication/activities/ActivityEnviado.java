ad785e017df3bb104d6f1f711c59812c2c18a7de
matheus <matheus.assuncao994@gmail.com>
2022-08-29 12:54:25 -0400

    card inserido em uma nova tela

---
+++ app/src/main/java/com/example/myapplication/activities/ActivityEnviado.java
@@ -0,0 +1,45 @@
+package com.example.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class ActivityEnviado extends AppCompatActivity {
+
+
+//    private View edtRegiao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enviado);
+
+//        edtRegiao=  findViewById(R.id.edtRegiao);
+//        edtRegiao.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                usarToast("Click");
+//            }
+//        });
+//
+//        edtRegiao.setOnLongClickListener(new View.OnLongClickListener() {
+//
+//            public boolean onLongClick(View v) {
+//
+//                usarToast("Long Click");
+//                return true;
+//            }
+//        });
+//    }
+//
+//        public void usarToast (String texto){
+//            Toast.makeText(this.getApplicationContext(), texto, Toast.LENGTH_LONG).show();
+//
+        }
+    }
